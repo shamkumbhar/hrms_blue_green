8a666ba0a1c4518474704afe339e24c6e37146d2
Ubuntu <ubuntu@ip-172-31-46-212.ap-south-1.compute.internal>
2025-09-08 06:59:02 +0000

    first commit

---
+++ frontendNew/dist/blue/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-IZ6KTALV.mjs";import"./chunk-IS3Y4M7G.mjs";import"./chunk-TSRJAL6Y.mjs";import"./chunk-XMS2YDBS.mjs";import"./chunk-CLVF3AUR.mjs";import"./chunk-7BWLUKWJ.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
---
+++ frontendNew/dist/blue/server/render-utils.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{c as o,d as r,e as n}from"./chunk-IS3Y4M7G.mjs";import"./chunk-TSRJAL6Y.mjs";import"./chunk-CLVF3AUR.mjs";import{Xb as e}from"./chunk-7BWLUKWJ.mjs";import"./chunk-FME56UVT.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
